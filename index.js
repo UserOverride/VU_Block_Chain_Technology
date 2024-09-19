c078fed48c514ce80c71d9a7ed2a8731023b7c20
UserOverride <laurynasdulskis@gmail.com>
2024-09-19 12:48:50 +0300

    up

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function makeid(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -20,53 +22,66 @@ function generateHash(input){
         59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 
         127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 
         191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 
-        257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317
+        257, 263, 269, 271, 277, 281, 283, 293, 307, 311
     ];
 
-    let curentIndex = 0;
-    for (let index = 0; index < inputArr.length; index++) {
-        if (curentIndex !== outputArr.length - 1) {
-            for (let secondIndex = curentIndex; secondIndex < outputArr.length; secondIndex++) {
-                outputArr[secondIndex] = outputArr[secondIndex] + inputArr[index];
-            }
-            curentIndex++;
-        }else{
-            curentIndex = 0;
-            index--;
+    function smallify(){
+        for (let index = 0; index < outputArr.length; index++) {
+           while (outputArr[index] > 10000) {
+            outputArr[index] = outputArr[index] / 100;
+           }
         }
     }
+
+    function doHash(){
+        let curentIndex = 0;
+        for (let index = 0; index < inputArr.length; index++) {
+            if (curentIndex !== outputArr.length - 1) {
+                for (let secondIndex = curentIndex; secondIndex < outputArr.length; secondIndex++) {
+                    outputArr[secondIndex] = outputArr[secondIndex] * inputArr[index];
+                }
+                smallify();
+                curentIndex++;
+            }else{
+                curentIndex = 0;
+                index--;
+            }
+        }
+    }
+
+    function createFinalHash(){
+        let finalHash = '';
+        for (let index = 0; index < outputArr.length; index++) {
+            let char = 48 + Math.floor(outputArr[index] % 74);
+            if (char > 57 && char < 65) {
+                char += 7;
+            }else if(char > 90 && char < 97){
+                char += 6;
+            }
+            finalHash += String.fromCharCode(char); 
+        }
+
+        return finalHash;
+    }
+
+    doHash();
 
     inputArr.sort((a, b) => b - a);
 
     outputArr.reverse();
 
-    curentIndex = 0;
-    for (let index = 0; index < inputArr.length; index++) {
-        if (curentIndex !== outputArr.length - 1) {
-            for (let secondIndex = curentIndex; secondIndex < outputArr.length; secondIndex++) {
-                outputArr[secondIndex] = outputArr[secondIndex] + inputArr[index];
-            }
-            curentIndex++;
-        }else{
-            curentIndex = 0;
-            index--;
-        }
-    }
+    doHash();
 
-    let finalHash = '';
-    for (let index = 0; index < outputArr.length; index++) {
-        let char = 48 + (outputArr[index] % 74);
-        if (char > 57 && char < 65) {
-            char += 7;
-        }else if(char > 90 && char < 97){
-            char += 6;
-        }
-        finalHash += String.fromCharCode(char); 
-    }
-
-    return finalHash;
+    return createFinalHash();
 }
 
-for (let index = 0; index < 1000; index++) {
-    console.log(`${index+1}: ${generateHash(makeid(1000))}`)
+
+let fail = false;
+for (let index = 0; index < 10000; index++) {
+    const text = makeid(1000);
+    console.log(`${index+1}: ${generateHash(text)}`);
+    if (generateHash(text) !== generateHash(text)) {
+        fail = true;
+    }
 }
+console.log(fail);
